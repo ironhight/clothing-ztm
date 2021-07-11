bd64a3420fa8d513e80ccd28aa664822eab3f6f7
ironhight <mrnguyenhoainam2468@gmail.com>
2021-07-11 14:58:10 +0000

    shop component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,19 +1,12 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
 
-export const selectCollections = createSelector(
-  [selectShop],
-  shop => shop.collections
+export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
+
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) => Object.values(collections));
+
+export const selectCollection = memoize((collectionUrlParam) =>
+	createSelector([selectCollections], (collections) => collections[collectionUrlParam]),
 );
-
-export const selectCollectionsForPreview = createSelector(
-  [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
-);
-
-export const selectCollection = collectionUrlParam =>
-  createSelector(
-    [selectCollections],
-    collections => collections[collectionUrlParam]
-  );
