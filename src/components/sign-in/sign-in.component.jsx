8ef67941b1d3bbf40e75df61798dafd424cde5d2
ironhight <mrnguyenhoainam2468@gmail.com>
2021-07-12 17:36:17 +0000

    move css to styled component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,103 +3,76 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
-import './sign-in.styles.scss';
-// import FacebookLogin from 'react-facebook-login';
-// import { GoogleLogin } from 'react-google-login';
-// import * as axios from 'axios';
+import {
+  SignInContainer,
+  SignInTitle,
+  ButtonsBarContainer
+} from './sign-in.styles';
 
 class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email: '',
-            password: '',
-        };
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-
-        this.setState({ email: '', password: '' });
+    this.state = {
+      email: '',
+      password: ''
     };
+  }
 
-    handleChange = (event) => {
-        const { value, name } = event.target;
+  handleSubmit = async event => {
+    event.preventDefault();
 
-        this.setState({ [name]: value });
-    };
+    const { email, password } = this.state;
 
-    // responseFacebook = async res => {
-    //     const rs = await axios.post(`${process.env.REACT_APP_HOST_API}/auth/customers/login/facebook`, {
-    //         accessToken: res.accessToken
-    //     })
-
-    //     if(!rs.data.status) return
-    //     window.localStorage.setItem('token', JSON.stringify(rs.data.data))  
-    // };
-
-    // onFailure = (error) => {
-    //     console.log("🚀 ~ file: sign-in.component.jsx ~ line 49 ~ SignIn ~ error", error)
-    // };
-
-    // googleResponse =  async res => {
-    //     const rs = await axios.post(`${process.env.REACT_APP_HOST_API}/auth/customers/login/google`, {
-    //         accessToken: res.accessToken
-    //     })
-
-    //     if(!rs.data.status) return
-    //     window.localStorage.setItem('token', JSON.stringify(rs.data.data))  
-    // }
-
-    render() {
-        return (
-            <div className="sign-in">
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput
-                        name="email"
-                        type="email"
-                        handleChange={this.handleChange}
-                        value={this.state.email}
-                        label="email"
-                        required
-                    />
-                    <FormInput
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                        handleChange={this.handleChange}
-                        label="password"
-                        required
-                    />
-                    <div className="buttons">
-                        <CustomButton type="submit"> Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                            Sign in with Google
-                        </CustomButton>
-                        {/* <FacebookLogin
-                            appId={process.env.REACT_APP_FACEBOOK_APP_ID}
-                            autoLoad={false}
-                            fields="name,email,picture"
-                            callback={this.responseFacebook}
-                        />
-                        <GoogleLogin
-                            className="GoogleLogin"
-                            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                            buttonText="Login"
-                            onSuccess={this.googleResponse}
-                            onFailure={this.onFailure}
-                        /> */}
-                    </div>
-                </form>
-            </div>
-        );
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <SignInContainer>
+        <SignInTitle>I already have an account</SignInTitle>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <ButtonsBarContainer>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </ButtonsBarContainer>
+        </form>
+      </SignInContainer>
+    );
+  }
 }
 
 export default SignIn;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
-
-// import MessengerCustomerChat from 'react-messenger-customer-chat';
+import { HomePageContainer } from './homepage.styles';
 
 const HomePage = () => (
-  <div className='homepage'>
-    <Directory />
-  </div>
+	<HomePageContainer>
+		<Directory />
+	</HomePageContainer>
 );
 
 export default HomePage;
