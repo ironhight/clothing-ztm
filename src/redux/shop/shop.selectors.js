840e934dc5b6b03734cfb10b63592ce14a8e6263
ironhight <mrnguyenhoainam2468@gmail.com>
2021-07-15 20:26:49 +0000

    add redux thunk & use container HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,18 +2,13 @@ import { createSelector } from 'reselect';
 
 const selectShop = (state) => state.shop;
 
-export const selectCollections = createSelector(
-  [selectShop],
-  (shop) => shop.collections
-);
+export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
 
-export const selectCollectionsForPreview = createSelector(
-  [selectCollections],
-  (collections) =>
-    collections ? Object.keys(collections).map((key) => collections[key]) : []
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) =>
+	collections ? Object.keys(collections).map((key) => collections[key]) : [],
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections ? collections[collectionUrlParam] : null
-  );
+	createSelector([selectCollections], (collections) => (collections ? collections[collectionUrlParam] : null));
+
+export const selectIsCollectionFetching = createSelector([selectShop], (shop) => shop.isFetching);
